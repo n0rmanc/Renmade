6a6cb56da510449bece83285cc027eeb36961db1
Norman Chen <s965202030@gmail.com>
2017-01-15 19:59:29 +0800

    Init

--- db/migrate/20170115102703_create_products.rb
+++ db/migrate/20170115102703_create_products.rb
@@ -3,11 +3,12 @@ class CreateProducts < ActiveRecord::Migration[5.0]
     create_table :products do |t|
       t.string :title, null: false
       t.string :shor_description, null: false
-      t.string :category, null: false
       t.string :sku, null: false
       t.boolean :visible, null: false, default: true
       t.integer :price
       t.integer :original_price
+      t.string :type
+      t.belongs :category
       t.timestamps
     end
   end
