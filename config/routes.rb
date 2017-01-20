97bf16a67ea79ac3f01b826661acf6b1de632d9a
Norman Chen <s965202030@gmail.com>
2017-01-20 09:28:45 +0800

    add product controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :main, only: :index
-  resources :products
+  resources :products do
+    member do
+      get :random
+    end
+  end
   root 'main#index'
 end
