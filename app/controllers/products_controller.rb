929f810705ea7352d80a117eec39a2bed7331ac2
Norman Chen <s965202030@gmail.com>
2017-02-12 13:56:28 +0800

    implement product show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,8 @@ class ProductsController < ApplicationController
     parent_product = DynamicProduct.find_by_id(params[:id])
     @static_product = parent_product.generate
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
 end
